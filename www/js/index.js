e98d4c9ec557c83d763f8719409c9269876b4ad0
caiobferreira <caaio.ferreira@gmail.com>
2021-03-26 15:35:40 -0300

    empty value validation

--- www/js/index.js
+++ www/js/index.js
@@ -10,11 +10,6 @@ function onDeviceReady() {
         value: 0,
       };
 
-      if(usdValue == ""){
-          console.log('this is empty');
-          return;
-      }
-
       let url =
         "https://free.currconv.com/api/v7/convert?q=USD_BRL&compact=ultra&apiKey=37d6b32bee7189bc371c";
 
@@ -24,6 +19,14 @@ function onDeviceReady() {
           let cotacao = data.USD_BRL;
           moedaB.value = (cotacao * parseFloat(usdValue)).toFixed(2);
           const resultado = moedaB.value;
+
+          if (usdValue == "") {
+            console.log("this is empty");
+            resultado = 0;
+            document.getElementById("usdCot").innerText = resultado;
+            return;
+          }
+
           document.getElementById("brlCot").innerText = resultado;
         });
       });
@@ -38,24 +41,26 @@ function onDeviceReady() {
         value: 0,
       };
 
-      if(usdValue == ""){
-          console.log('this is empty');
-          return;
-      }
-
       let url =
         "https://free.currconv.com/api/v7/convert?q=BRL_USD&compact=ultra&apiKey=37d6b32bee7189bc371c";
 
       fetch(url).then((res) => {
         res.json().then((data) => {
           console.log("data", data);
+
           let cotacao = data.BRL_USD;
           moedaB.value = (cotacao * parseFloat(brlValue)).toFixed(2);
-          const resultado = moedaB.value;
+          let resultado = moedaB.value;
+
+          if (brlValue == "") {
+            console.log("this is empty");
+            resultado = 0;
+            document.getElementById("usdCot").innerText = resultado;
+            return;
+          }
+
           document.getElementById("usdCot").innerText = resultado;
         });
       });
     });
-
-
 }
